a831f261f5ed6e3ca00016d0a0281ea39e9af19e
dvdr1029 <67302404+dvdr1029@users.noreply.github.com>
2024-12-07 13:49:47 -0500

    day 3 and code documentation

--- code_files/day1.py
+++ code_files/day1.py
@@ -1,6 +1,21 @@
+"""Day 1 of Advent of Code 2024.
+The goal of part 1 is to find the sum of the absolute differences between the
+two lists of integers after the lists have been sorted from least to greatest.
+
+The goal of part 2 is to find the sum of each integer in the first list
+multiplied by the count of its appearance in the second list. By using
+dictionaries as counters, we can quickly multiply each unique integer in the
+left list by it's occurance in the left list and the right list to not make
+repeated calls to the right lists dictionary count.
+"""
+
+
 class AdventDay1:
 
     def __init__(self):
+        """Read in the input file and sort the two lists and create counters.
+        The counters will be used to multiply the integer by the count of its
+        appearance in each list."""
         self.left = []
         self.right = []
         with open('input_files/day1input.txt', 'r') as f:
@@ -16,14 +31,18 @@ class AdventDay1:
         self.part2 = 0
 
     def get_diff_sum(self):
+        """Get the sum of the absolute differences between the two lists."""
         for i in range(len(self.left)):
             self.part1 += abs(self.left[i] - self.right[i])
 
     def get_similarity_score(self):
+        """Get the sum of each integer in the left list multiplied by the count
+        of its appearance in the right list."""
         for key, val in self.left_counter.items():
             self.part2 += key * val * self.right_counter.get(key, 0)
 
     def _val_counter(self, list):
+        """Create a dictionary of the count of each integer in the list."""
         counts = {}
         for x in list:
             counts[x] = counts.get(x, 0) + 1
--- code_files/day2.py
+++ code_files/day2.py
@@ -1,6 +1,26 @@
+"""Day 2 of Advent of Code 2024.
+
+The goal of part 1 is to find the number of rows in the input file that are
+"safe". A row is considered safe if the absolute difference between each
+integer in the row is less than or equal to 3 and that any row is monotonicly
+increasing or decreasing.
+
+We can use a set to store the differences
+between each integer in the row and check if the set is a subset of the set of
+differences that are less strictly between 1 and 3 in a monotonic direction.
+
+The goal of part 2 is to find the number of rows in the input file that are
+"safe" and to allow for one integer in the row to be removed to make the row
+"safe". We can use the same method as part 1 to find the safe rows and then
+iterate over the rows that are not safe to see if removing one integer makes
+the row safe.
+"""
+
+
 class AdventDay2:
 
     def __init__(self):
+        """Read in the input file and store the data in a list of lists."""
         self.data = []
         self.unsafe = []
         with open('input_files/day2input.txt', 'r') as f:
@@ -10,6 +30,8 @@ class AdventDay2:
         self.part2 = 0
 
     def find_safe_reports(self):
+        """Find the number of safe rows in the input file and also stores
+        any unsafe row's index in a list for part 2."""
         for i, row in enumerate(self.data):
             if self._is_safe_row(row):
                 self.part1 += 1
@@ -18,6 +40,8 @@ class AdventDay2:
                 self.unsafe.append(i)
 
     def allow_one_bad_level(self):
+        """Iterate over the unsafe rows and check if removing one integer
+        makes the row safe."""
         for index in self.unsafe:
             row = self.data[index]
             for i in range(len(row)):
@@ -26,6 +50,9 @@ class AdventDay2:
                     break
 
     def _is_safe_row(self, row):
+        """Check if the row is safe by checking if the absolute difference
+        between each integer in the row is less than or equal to 3 and that
+        the row is monotonicly increasing or decreasing."""
         self.row_diff_set = {row[j] - row[j-1] for j in range(1, len(row))}
         return (self.row_diff_set <= {1, 2, 3} or
                 self.row_diff_set <= {-1, -2, -3})
